2d073928ff42a91b3252d680fbad690f904f2608
Derek Wan <d.wan@icloud.com>
2023-06-03 22:13:31 +0800

    Finish lesson 8 (build a REST API)

--- src/app/api/echo/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const obj = Object.fromEntries(searchParams.entries());
-  return NextResponse.json(obj);
-}
